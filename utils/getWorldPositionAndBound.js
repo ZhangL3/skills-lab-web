f34cc2cde256d1399fa4b294a4e71dea5ba27454
zhanglebielefeld <zhanglebielefeld@gmail.com>
2018-07-22 16:13:18 +0200

    Remove redundance

--- utils/getWorldPositionAndBound.js
+++ utils/getWorldPositionAndBound.js
@@ -1,18 +1,11 @@
 import $ from 'jquery';
 
 let element;
-let model;
 
 export default AFRAME.registerComponent('get_world_bound', {
 
     init: function(){
         element = this.el;
-
-        // let boundingBoxInInit = new THREE.Box3().setFromObject(element.object3D);
-        // console.log("boundingBoxInInit: ", boundingBoxInInit, typeof(boundingBoxInInit));
-
-
-        // console.log("test: ", element.getAttribute('worldBoundMaxX'));
 
         $(this.el).on('click', () => {
         });
@@ -32,14 +25,13 @@ function getWordPosition() {
 
 function getWorldBound() {
     let boundingBox = new THREE.Box3().setFromObject(element.object3D);
-    console.log("element.object3D: ", element.object3D, typeof(element.object3D));
+
     $(element).attr('worldBoundMaxX', boundingBox.max.x);
     $(element).attr('worldBoundMaxY', boundingBox.max.y);
     $(element).attr('worldBoundMaxZ', boundingBox.max.z);
     $(element).attr('worldBoundMinX', boundingBox.min.x);
     $(element).attr('worldBoundMinY', boundingBox.min.y);
     $(element).attr('worldBoundMinZ', boundingBox.min.z);
-
 
     console.log("element got wordBound: ", element);
 }
