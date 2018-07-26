800c017f8dbd9f03811d7800e7906c19b2557047
zhanglebielefeld <zhanglebielefeld@gmail.com>
2018-07-26 15:41:57 +0200

    Add function to judge the trigger event.

---
+++ utils/isEmitted.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+
+export function isEmitted(element, triggerPosition) {
+    const el = $(element);
+
+    const maxX = Number(el.attr('worldboundmaxx'));
+    const maxY = Number(el.attr('worldboundmaxy'));
+    const maxZ = Number(el.attr('worldboundmaxz'));
+
+    const minX = Number(el.attr('worldboundminx'));
+    const minY = Number(el.attr('worldboundminy'));
+    const minZ = Number(el.attr('worldboundminz'));
+
+    console.log(maxX, maxY, maxZ, minX, minY, minZ);
+
+    if (triggerPosition.x > maxX || triggerPosition.x < minX) {
+        console.log("x wrong");
+        return false;
+    }
+
+    if (triggerPosition.y > maxY || triggerPosition.y < minY) {
+        console.log("y wrong");
+        return false;
+    }
+
+    if (triggerPosition.z > maxZ || triggerPosition.z < minZ) {
+        console.log("z wrong");
+        return false;
+    }
+
+    return true;
+}
