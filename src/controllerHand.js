e35ca589ddbf4ef8e00b5fde01f91586de6809df
zhanglebielefeld <zhanglebielefeld@gmail.com>
2018-09-20 17:02:25 +0200

    Add animated hand

--- src/controllerHand.js
+++ src/controllerHand.js
@@ -7,12 +7,20 @@ import controllerStateIndex from '../utils/controllerState';
 let controllerRightHand;
 let controllerLeftHand;
 
+let controllerRight;
+let controllerLeft;
+
+let opacityByGrasping = 0.2;
+
 export default AFRAME.registerComponent('controller_hand', {
 
     init: function(){
         // shallow copy
         controllerRightHand = document.querySelector('#rightHand');
         controllerLeftHand = document.querySelector('#leftHand');
+
+        controllerRight = document.querySelector('#viveControllerRight');
+        controllerLeft = document.querySelector('#viveControllerLeft');
 
         // deep copy
         // currentState = _.cloneDeep(stateIndex.getState());
@@ -22,6 +30,13 @@ export default AFRAME.registerComponent('controller_hand', {
 
         $(controllerRightHand).on('click', () => {
             controllerRightHand.setAttribute("animation-mixer", "loop: once");
+            if (haveSthInHand(controllerRight)) {
+                console.log("something in right hand!!!");
+                setHandOpacity(controllerRightHand, opacityByGrasping);
+            } else {
+                controllerRightHand.removeAttribute("material");
+                setHandOpacity(controllerRightHand, 1);
+            }
         });
 
         controllerRightHand.addEventListener('animation-finished', ()=>{
@@ -30,6 +45,13 @@ export default AFRAME.registerComponent('controller_hand', {
 
         $(controllerLeftHand).on('click', () => {
             controllerLeftHand.setAttribute("animation-mixer", "loop: once");
+            if (haveSthInHand(controllerLeft)) {
+                console.log("something in left hand!!!");
+                setHandOpacity(controllerLeftHand, opacityByGrasping);
+            } else {
+                controllerLeftHand.removeAttribute("material");
+                setHandOpacity(controllerLeftHand, 1);
+            }
         });
 
         controllerLeftHand.addEventListener('animation-finished', ()=>{
@@ -37,6 +59,46 @@ export default AFRAME.registerComponent('controller_hand', {
         });
     }
 });
+
+function setHandOpacity(handElement, opacity) {
+
+    const mesh = handElement.getObject3D('mesh'); // For hand, the result is undefined
+    if (!mesh) { return; }
+    mesh.traverse(function (node) {
+        if (node.isMesh) {
+            node.material.opacity = opacity;
+            node.material.transparent = opacity < 1.0;
+            node.material.needsUpdate = true;
+            node.material.alphaTest = 0.1;
+
+            console.log("node.material: ", node.material, typeof(node.material));
+        }
+    });
+}
+
+function haveSthInHand(controllerElement){
+    const children = controllerElement.childNodes;
+    for (let i=0; i<children.length; i++) {
+        // console.log("i: ", i, typeof(i));
+        // console.log("children[i]: ", children[i], typeof(children[i]));
+        // console.log("children[i].nodeType === 1: ", children[i].nodeType === 1, typeof(children[i].nodeType === 1));
+        // if(children[i].nodeType===1) {
+        //     console.log("children[i].getAttribute('id'): ", children[i].getAttribute('id'), typeof(children[i].getAttribute('id')));
+        //     console.log("children[i].getAttribute('id') !== 'leftHand': ", children[i].getAttribute('id') !== 'leftHand', typeof(children[i].getAttribute('id') !== 'leftHand'));
+        //     console.log("children[i].getAttribute('id') !== 'rightHand': ", children[i].getAttribute('id') !== 'rightHand', typeof(children[i].getAttribute('id') !== 'rightHand'));
+        //     console.log("children[i].getAttribute('visible'): ", children[i].getAttribute('visible'), typeof(children[i].getAttribute('visible')));
+        // }
+        if (
+            children[i].nodeType === 1
+            && children[i].getAttribute('id') !== 'leftHand'
+            && children[i].getAttribute('id') !== 'rightHand'
+            && children[i].getAttribute('visible')
+        ) {
+            return true;
+        }
+    }
+    return false;
+}
 
 export function handleNotifyControllerHand(nextState) {
 
@@ -51,7 +113,6 @@ export function handleControllerNotifyControllerHand ( triggerEvent ) {
     else if (triggerEvent.activeController.getAttribute('id')==='viveControllerLeft') {
         $(controllerLeftHand).trigger('click');
     }
-
 }
 
 export function handleControllerStateNotifyControllerHand (nextControllerState) {
