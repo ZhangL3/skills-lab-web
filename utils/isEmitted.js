cf3ffe970da0b4bf30a6e87cad9e62279e4affdc
zhanglebielefeld <zhanglebielefeld@gmail.com>
2018-10-04 11:25:25 +0200

    Make isEmitted better, add collision detection

--- utils/isEmitted.js
+++ utils/isEmitted.js
@@ -1,15 +1,26 @@
 import $ from 'jquery';
+import {getWorldBound, getVertexesOfBoundingBox} from './getWorldPositionAndBound';
 
 export function isEmitted(element, triggerPosition, modifier = 0) {
     const el = $(element);
 
-    const maxX = Number(el.attr('worldboundmaxx'));
-    const maxY = Number(el.attr('worldboundmaxy'));
-    const maxZ = Number(el.attr('worldboundmaxz'));
+    const boundingBox = new THREE.Box3().setFromObject(element.object3D);
 
-    const minX = Number(el.attr('worldboundminx'));
-    const minY = Number(el.attr('worldboundminy'));
-    const minZ = Number(el.attr('worldboundminz'));
+    // const maxX = Number(el.attr('worldboundmaxx'));
+    // const maxY = Number(el.attr('worldboundmaxy'));
+    // const maxZ = Number(el.attr('worldboundmaxz'));
+    //
+    // const minX = Number(el.attr('worldboundminx'));
+    // const minY = Number(el.attr('worldboundminy'));
+    // const minZ = Number(el.attr('worldboundminz'));
+
+    const maxX = boundingBox.max.x;
+    const maxY = boundingBox.max.y;
+    const maxZ = boundingBox.max.z;
+
+    const minX = boundingBox.min.x;
+    const minY = boundingBox.min.y;
+    const minZ = boundingBox.min.z;
 
     if (triggerPosition.x > maxX - modifier || triggerPosition.x < minX + modifier) {
         // console.log("x wrong");
@@ -55,3 +66,23 @@ export function isEmittedBoundingBox(boundingBox, position) {
 
     return true;
 }
+
+export function detectCollision(elementObject, elementTarget) {
+    const objectVertexes = getVertexesOfBoundingBox(elementObject);
+    const targetVertexes = getVertexesOfBoundingBox(elementTarget);
+
+    for ( let i = 0; i < objectVertexes.length; i++ ) {
+        if ( isEmitted(elementTarget, objectVertexes[i]) ) {
+            return true;
+        }
+    }
+
+    for ( let j = 0; j < targetVertexes.length; j++) {
+        if ( isEmitted(elementObject, targetVertexes[j]) ) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
