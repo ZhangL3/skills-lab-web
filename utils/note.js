cac72429b04fac76ce1d466cba056bffc8023fda
zhanglebielefeld <zhanglebielefeld@gmail.com>
2018-11-01 17:04:24 +0100

    Hint for the exercise

--- utils/note.js
+++ utils/note.js
@@ -1,18 +1,25 @@
 export default note = {
-    // information of objects
-    tips: {
-        portfolio: 'check 5 R',
-        bottle: 'check label, cap and liquid',
-        infusionSetInPack: 'check label',
-        infusionSetOpen: 'close roller clamp',
-        infusionSetHanged: 'squeeze drip chamber, open roller clamp, fix tube'
-    },
-    // information of progress
-    hints: {
-        portfolio: 'check portfolio',
-        deskDisinfection: 'disinfect desk',
-        handDisinfection: 'disinfect hands',
-        infusionSetHanged: 'fix tube',
-        pasteNameLabel: 'past name label'
-    }
+    checkPortfolio: 'Check portfolio',
+    check5R: 'Check 5 R',
+    handDisinfection: 'Hand disinfection',
+    deskDisinfection: 'Desk disinfection',
+    wearGloves: 'Wear gloves',
+    takeDisinfectionCloth: 'Take disinfection clothes',
+    takeOffGloves: 'Take off the gloves',
+    prepareInjection: 'Prepare injection',
+    takeInfusionSet: 'Take infusion set',
+    checkInfusionSet: 'Check infusion set',
+    closeRoller: 'Close the roller',
+    takeOffInfusionSetCap: 'Take off the cap',
+    takeDrug: 'Take drug',
+    checkDrug: 'Check drug',
+    takeOffBottleCap: 'Take off the cap',
+    pierceInfusionSet: 'Pierce infusion set into bottle',
+    hang: 'Hang the bottle of drug',
+    squeezeChamber: 'Squeeze the Chamber',
+    openRoller: 'Open the roller',
+    fixTube: 'Fix the tube',
+    takeNameLabel: 'Take one name label',
+    fillNameLabel: 'Fill the name label',
+    pastNameLabel: 'Past name label'
 }
