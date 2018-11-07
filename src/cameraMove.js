cb027d8feda943029fafd63f19f031880f959b9f
zhanglebielefeld <zhanglebielefeld@gmail.com>
2018-11-07 14:47:46 +0100

    Correct remove event listener for Vive version

--- src/cameraMove.js
+++ src/cameraMove.js
@@ -13,6 +13,7 @@ let infusionSetOpen;
 let infusionSetHanged;
 let infusionSetHangedFilled;
 let handDisinfectionHandle;
+let element;
 
 const positionVar = {
     origin: {name: 'ORIGIN', position: '0 1 -0.4'},
@@ -30,7 +31,7 @@ AFRAME.registerComponent('camera-move', {
 
     init: function () {
         let data = this.data;
-        const el = this.el;
+        element = this.el;
 
         if (data.disable) {
             return false;
@@ -44,163 +45,143 @@ AFRAME.registerComponent('camera-move', {
         infusionSetHanged = document.querySelector('#infusionSetHanged');
         infusionSetHangedFilled = document.querySelector('#infusionSetHangedFilled');
         handDisinfectionHandle = document.querySelector('#handDisinfectionHandle');
-        let timer;
-
-        //test
 
         // move to cupboard
-        nacl500Bottle.addEventListener('raycaster-intersected', ()=> {
-            this.moveToCupboardAndBack(el, data.disable)
-        });
+        nacl500Bottle.addEventListener('raycaster-intersected', moveToCupboardAndBack);
 
         // move to cabinet
-        infusionSetOpen.addEventListener('raycaster-intersected', ()=> {
-            this.moveToCabinetAndBack(el, data.disable)
-        });
+        infusionSetOpen.addEventListener('raycaster-intersected', moveToCabinetAndBack);
 
         // move to holder
-        infusionSetHanged.addEventListener('raycaster-intersected', ()=> {
-            this.moveToHolderAndBack(el, data.disable)
-        });
+        infusionSetHanged.addEventListener('raycaster-intersected', moveToHolderAndBack);
 
         // move to holder
-        infusionSetHangedFilled.addEventListener('raycaster-intersected', ()=> {
-            this.moveToHolderAndBack(el, data.disable)
-        });
+        infusionSetHangedFilled.addEventListener('raycaster-intersected', moveToHolderAndBack);
 
         // move to hand disinfection
-        handDisinfectionHandle.addEventListener('raycaster-intersected', ()=> {
-            this.moveToHandDisinfectionAndBack(el, data.disable)
-        });
+        handDisinfectionHandle.addEventListener('raycaster-intersected', moveToHandDisinfectionAndBack);
     },
 
     update: function (oldData) {
         let data = this.data;
-        let el = this.el;
 
         if (data.disable) {
-            // Remove raycaster-intersected listener
-            nacl500Bottle = document.querySelector('#nacl500Bottle');
-            infusionSetOpen = document.querySelector('#infusionSetOpen');
-            infusionSetHanged = document.querySelector('#infusionSetHanged');
-            infusionSetHangedFilled = document.querySelector('#infusionSetHangedFilled');
-            handDisinfectionHandle = document.querySelector('#handDisinfectionHandle');
 
             // move to cupboard
-            nacl500Bottle.removeEventListener('raycaster-intersected', this.moveToCupboardAndBack);
+            nacl500Bottle.removeEventListener('raycaster-intersected', moveToCupboardAndBack);
 
             // move to cabinet
-            infusionSetOpen.removeEventListener('raycaster-intersected', this.moveToCabinetAndBack);
+            infusionSetOpen.removeEventListener('raycaster-intersected', moveToCabinetAndBack);
 
             // move to holder
-            infusionSetHanged.removeEventListener('raycaster-intersected', this.moveToHolderAndBack);
+            infusionSetHanged.removeEventListener('raycaster-intersected', moveToHolderAndBack);
 
             // move to holder
-            infusionSetHangedFilled.removeEventListener('raycaster-intersected', this.moveToHolderAndBack);
+            infusionSetHangedFilled.removeEventListener('raycaster-intersected', moveToHolderAndBack);
 
             // move to hand disinfection
-            handDisinfectionHandle.removeEventListener('raycaster-intersected', this.moveToHandDisinfectionAndBack);
+            handDisinfectionHandle.removeEventListener('raycaster-intersected', moveToHandDisinfectionAndBack);
         }
 
     },
-
-    moveToCupboardAndBack: function(el, disable) {
-        console.log("disable: ", disable, typeof(disable));
-        if(cameraMoved || disable) {
-            return false;
-        }
-        if (
-            stateIndex.get('started')
-            && stateIndex.getIn(['bottlePrepare', 'position']) === constants.bottle.position.IN_CUPBOARD
-            && cameraPosition === positionVar.origin.name
-        ) {
-            this.moveToBottle(el);
-            let timer = setTimeout(()=>{
-                this.backToOrigin(el);
-            }, timeToStay);
-        }
-    },
-
-    moveToCabinetAndBack: function (el, disable) {
-        if(cameraMoved || disable) {
-            return false;
-        }
-        if (
-            stateIndex.getIn(['infusionSet', 'position']) === constants.infusionSet.position.ON_TABLE
-            && (
-                stateIndex.getIn(['infusionSet', 'withCap'])
-                || stateIndex.getIn(['infusionSet', 'rollerClapOpen'])
-            )
-            && cameraPosition === positionVar.origin.name
-        ) {
-            this.moveToInfusionSetOpen(el);
-            let timer = setTimeout(()=>{
-                this.backToOrigin(el);
-            }, timeToStay);
-        }
-    },
-
-    moveToHolderAndBack: function (el, disable) {
-        if(cameraMoved || disable) {
-            return false;
-        }
-        if (
-            stateIndex.getIn(['bottlePrepare', 'position']) === constants.bottle.position.HANGED
-            && cameraPosition === positionVar.origin.name
-        ) {
-            this.moveToInfusionSetHanged(el);
-            let timer = setTimeout(()=>{
-                this.backToOrigin(el);
-            }, timeToStay);
-        }
-    },
-
-    moveToHandDisinfectionAndBack: function (el, disable) {
-        if(cameraMoved || disable) {
-            return false;
-        }
-        if (
-            (stateIndex.getIn(['handDisinfection', 'finish']) === 0
-                && stateIndex.getIn(['portfolio', 'finish']))
-            ||
-            (stateIndex.getIn(['handDisinfection', 'finish']) === 1
-                && stateIndex.getIn(['tableDisinfection', 'finish']))
-            && cameraPosition === positionVar.origin.name
-        ) {
-            this.moveToHandDisinfection(el);
-            let timer = setTimeout(()=>{
-                this.backToOrigin(el);
-            }, timeToStay);
-        }
-    },
-
-    backToOrigin: function(el) {
-        aAnimationWrapper(el, '', 'position', '', positionVar.origin.position, moveTime, '', true, 'forwards');
-        cameraPosition = positionVar.origin.name;
-        cameraMoved = false;
-    },
-
-    moveToBottle: function (el) {
-        aAnimationWrapper(el, '', 'position', '', positionVar.nacl500Bottle.position, moveTime, '', true, 'forwards');
-        cameraPosition = positionVar.nacl500Bottle.name;
-        cameraMoved = true;
-    },
-
-    moveToInfusionSetOpen: function (el) {
-        aAnimationWrapper(el, '', 'position', '', positionVar.infusionSetOpen.position, moveTime, '', true, 'forwards');
-        cameraPosition = positionVar.infusionSetOpen.name;
-        cameraMoved = true;
-    },
-
-    moveToInfusionSetHanged: function (el) {
-        aAnimationWrapper(el, '', 'position', '', positionVar.infusionSetHanged.position, moveTime, '', true, 'forwards');
-        cameraPosition = positionVar.infusionSetHanged.name;
-        cameraMoved = true;
-    },
-
-    moveToHandDisinfection: function (el) {
-        aAnimationWrapper(el, '', 'position', '', positionVar.handDisinfectionHandle.position, moveTime, '', true, 'forwards');
-        cameraPosition = positionVar.handDisinfectionHandle.name;
-        cameraMoved = true;
-    }
 });
+
+function moveToCupboardAndBack() {
+    if(cameraMoved) {
+        return false;
+    }
+    if (
+        stateIndex.get('started')
+        && stateIndex.getIn(['bottlePrepare', 'position']) === constants.bottle.position.IN_CUPBOARD
+        && cameraPosition === positionVar.origin.name
+    ) {
+        moveToBottle(element);
+        let timer = setTimeout(()=>{
+            backToOrigin(element);
+        }, timeToStay);
+    }
+}
+
+function moveToCabinetAndBack() {
+    if(cameraMoved) {
+        return false;
+    }
+    if (
+        stateIndex.getIn(['infusionSet', 'position']) === constants.infusionSet.position.ON_TABLE
+        && (
+            stateIndex.getIn(['infusionSet', 'withCap'])
+            || stateIndex.getIn(['infusionSet', 'rollerClapOpen'])
+        )
+        && cameraPosition === positionVar.origin.name
+    ) {
+        moveToInfusionSetOpen(element);
+        let timer = setTimeout(()=>{
+            backToOrigin(element);
+        }, timeToStay);
+    }
+}
+
+function moveToHolderAndBack() {
+    if(cameraMoved) {
+        return false;
+    }
+    if (
+        stateIndex.getIn(['bottlePrepare', 'position']) === constants.bottle.position.HANGED
+        && cameraPosition === positionVar.origin.name
+    ) {
+        moveToInfusionSetHanged(element);
+        let timer = setTimeout(()=>{
+            backToOrigin(element);
+        }, timeToStay);
+    }
+}
+
+function moveToHandDisinfectionAndBack() {
+    if(cameraMoved) {
+        return false;
+    }
+    if (
+        (stateIndex.getIn(['handDisinfection', 'finish']) === 0
+            && stateIndex.getIn(['portfolio', 'finish']))
+        ||
+        (stateIndex.getIn(['handDisinfection', 'finish']) === 1
+            && stateIndex.getIn(['tableDisinfection', 'finish']))
+        && cameraPosition === positionVar.origin.name
+    ) {
+        moveToHandDisinfection(element);
+        let timer = setTimeout(()=>{
+            backToOrigin(element);
+        }, timeToStay);
+    }
+}
+
+function backToOrigin(el) {
+    aAnimationWrapper(el, '', 'position', '', positionVar.origin.position, moveTime, '', true, 'forwards');
+    cameraPosition = positionVar.origin.name;
+    cameraMoved = false;
+}
+
+function moveToBottle(el) {
+    aAnimationWrapper(el, '', 'position', '', positionVar.nacl500Bottle.position, moveTime, '', true, 'forwards');
+    cameraPosition = positionVar.nacl500Bottle.name;
+    cameraMoved = true;
+}
+
+function moveToInfusionSetOpen(el) {
+    aAnimationWrapper(el, '', 'position', '', positionVar.infusionSetOpen.position, moveTime, '', true, 'forwards');
+    cameraPosition = positionVar.infusionSetOpen.name;
+    cameraMoved = true;
+}
+
+function moveToInfusionSetHanged(el) {
+    aAnimationWrapper(el, '', 'position', '', positionVar.infusionSetHanged.position, moveTime, '', true, 'forwards');
+    cameraPosition = positionVar.infusionSetHanged.name;
+    cameraMoved = true;
+}
+
+function moveToHandDisinfection(el) {
+    aAnimationWrapper(el, '', 'position', '', positionVar.handDisinfectionHandle.position, moveTime, '', true, 'forwards');
+    cameraPosition = positionVar.handDisinfectionHandle.name;
+    cameraMoved = true;
+}
+
